27b497562890e911a0e95ba0378152edd614de48
Daniel Efrén Velázquez Lara <danielvelara@gmail.com>
2021-02-26 21:18:00 -0600

    Practica 1

--- xv6-public/sysproc.c
+++ xv6-public/sysproc.c
@@ -89,3 +89,15 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+// Llamada a sistema para apagar la computadora
+
+int sys_shutdown(void){
+  outw(0x604, 0x2000);
+  return 0;
+}
+
+int sys_reboot(void){
+  outb(0x64, 0xFE);
+  return 0;
+}
