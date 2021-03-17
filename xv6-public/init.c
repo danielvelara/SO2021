3b384c4ecd65f0d0aeab3b2007e83580de04c2a8
Daniel Efrén Velázquez Lara <danielvelara@gmail.com>
2021-03-16 19:31:19 -0600

    Practica 2

--- xv6-public/init.c
+++ xv6-public/init.c
@@ -20,13 +20,16 @@ main(void)
   dup(0);  // stderr
 
   for(;;){
-    printf(1, "init: starting sh\n");
+    printf(1, "init: Starting sh\n");
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
       exit();
     }
     if(pid == 0){
+	//Con ls: List all program and files
+	//Print infinitely
+	// If program does not exist, then in fails execution and entres a loop
       exec("sh", argv);
       printf(1, "init: exec sh failed\n");
       exit();
